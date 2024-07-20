b43553c766a29bd5dfdccc31247afc22f35e1a8a
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-07-20 06:59:12 -0400

    Updated resolution to 1920 by 1080

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(320, 180, PlayState, 60, 60, true));
+		addChild(new FlxGame(1920, 1080, PlayState, 60, 60, true));
 	}
 }
